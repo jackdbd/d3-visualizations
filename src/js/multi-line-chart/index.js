2458feff5e940057402119aa63c88f910056212c
Giacomo Debidda <jackdebidda@gmail.com>
2019-04-27 14:37:42 +0200

    Fix url for unemployment.tsv in multi-line-chart

--- src/js/multi-line-chart/index.js
+++ src/js/multi-line-chart/index.js
@@ -1,7 +1,8 @@
 import { ROOT_SELECTOR_ID } from '../utils';
 import { fn } from './multi-line-chart.ts';
 
-const url = 'https://raw.githubusercontent.com/jackdbd/d3-visualizations/typescript-viz/src/data/unemployment.tsv';
+const url =
+  'https://raw.githubusercontent.com/jackdbd/d3-visualizations/master/src/data/unemployment.tsv';
 
 fn(ROOT_SELECTOR_ID, url);
 
